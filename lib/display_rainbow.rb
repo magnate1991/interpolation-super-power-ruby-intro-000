b7faca0184726d66b77e3be3033228fc8f64314b
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-08 10:45:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 def display_rainbow(colors)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'] 
   letters = [ "R", "O" , "Y" , "G", "B" , "I" , "V" ] 
-  puts 
-  colo
+  puts " Color #{letter[0]}"
+  
 end 
 

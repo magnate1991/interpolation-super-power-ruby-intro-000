06c1069c590377c1557c75140eb772a4ca431bd1
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-08 11:11:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,6 @@ def
    
   puts " R:  #{colors[0]}, O:  #{colors[1]}, Y:  #{colors[2]}, G:  #{colors[3]}, B:  #{colors[4]}, I:  #{colors[5]}, V:  #{colors[6]}"
 end
-end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'] 
 

8f0e2948351be63ffd92eff53ebd3f161a749ec7
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-08 10:44:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'] 
+  letters = [ "R", "O" , "Y" , "G", "B" , "I" , "V" ] 
   puts 
+  colo
 end 
 
